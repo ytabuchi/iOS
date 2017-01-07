58453ba599e64a465ed7d72655a18b7a206ac4b1
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-01-07 14:24:28 +0900

    adding some simple tableviews

---
+++ UITableViewSample/UITableViewSample/UITableViewSample.iOS/DefaultTableViewController.cs
@@ -0,0 +1,23 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace UITableViewSample.iOS
+{
+    public partial class DefaultTableViewController : UITableViewController
+    {
+        public DefaultTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            string[] items = { "test1", "test2", "test3" };
+
+            DefaultTableSource.Source = new TableSource(items);
+            DefaultTableSource.ReloadData();
+        }
+    }
+}
--- UITableViewSample/UITableViewSample/UITableViewSample.iOS/TableSource.cs
+++ UITableViewSample/UITableViewSample/UITableViewSample.iOS/TableSource.cs
@@ -6,33 +6,25 @@ using UIKit;
 
 namespace UITableViewSample.iOS
 {
-    public class TableSource : UITableViewSource
+public class TableSource : UITableViewSource
+{
+    public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
     {
-        protected string[] tableItems;
-        protected string cellIdentifier = "TableCell";
+        throw new NotImplementedException();
+    }
 
-        public TableSource(string[] tableitems)
-        {
-            this.tableItems = tableitems;
-        }
-
-        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
-        {
-            UITableViewCell cell = tableView.DequeueReusableCell(cellIdentifier);
-            string item = tableItems[indexPath.Row];
-
-            // 再利用できるセルがなければ新規作成する。
-            if (cell == null)
-                cell = new UITableViewCell(UITableViewCellStyle.Default, cellIdentifier);
-
-            cell.TextLabel.Text = item;
-
-            return cell;
-        }
-
-        public override nint RowsInSection(UITableView tableview, nint section)
-        {
-            return tableItems.Length;
-        }
+    public override nint RowsInSection(UITableView tableview, nint section)
+    {
+        throw new NotImplementedException();
     }
 }
+}
+
+
+
+
+
+
+
+
+
